58f6d8b1efa43af8e094b8e1c7be9f8ef46c44a3
leotfs <leotelles.123@gmail.com>
2022-06-01 22:11:02 -0300

    att

--- config/dev.exs
+++ config/dev.exs
@@ -10,7 +10,7 @@ config :aulas_cria, AulasCriaWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   # http: [ip: {127, 0, 0, 1}, port: 4000],
-  http: [ip: {127, 0, 0, 1}, port: 80],
+  http: [ip: {127, 0, 0, 1}],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
